7ff855c65b8ab61bbef297dfea82e186e6cfacf8
Micha Mazaheri <micha@mazaheri.me>
2016-11-11 15:48:28 +0100

    Add copy option and refactor

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# pawprint-cli
+# Pawprint CLI
+
 A Node.js utility tool to create and manage Pawprints HTTP traces.
 
 ## Example
@@ -10,3 +11,25 @@ curl  --trace - https://httpbin.org/get | pawpt
 ```
 
 <img src="https://cl.ly/1b1u2z0E002w/Pawprint%20CLI%20Example@2x.png" width="641" />
+
+This will allow you to view on a nice webpage or share it as a short link.
+
+<img src="https://cl.ly/2e1a1J0K2M19/Pawprint%202%20Blog%20Article%20(1)@2x.png" width="688" alt="Share HTTP traces in a beautiful UI via Pawprint by Paw" />
+
+## Options
+
+- `-p, --private` Create a private Pawprint. This means the Pawprint will be created with an access token, so only people who you share the full URL (including access token) with, will be able to access the data. Also, this means the Pawprint won't be indexed by search engines.
+- `-c, --copy` Copies the public share link in the clipboard so it's ready to paste in another app.
+
+## Dependencies
+
+This tool is based one a few other open source projects, mostly:
+
+- [curl-trace-parser](https://github.com/apiaryio/curl-trace-parser) Parser for output from Curl --trace option
+- [chalk](https://github.com/chalk/chalk) Terminal string styling done right
+- [request](https://github.com/request/request) Simplified HTTP request client.
+
+## License
+
+This repository is released under the [MIT License](LICENSE). Feel free to fork, and modify!
+Copyright © 2016 Paw Inc.
--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 #!/usr/bin/env node
 
-var cmd = require('commander');
-var fs = require('fs');
-var process = require('process');
 var parser = require('curl-trace-parser');
 var request = require('request');
-var chalk = require('chalk');
 var log = console.log;
 
-var logPawprintCreation = function(pawprintData) {
-  log('')
-  log(chalk.green.bold('  ✓ Pawprint Created'));
+var logPawprintCreation = function(pawprintData, isCopied) {
+  var chalk = require('chalk');
+  log('');
+  log(chalk.bold.green('  ✓ Pawprint Created') + ' ' +
+      (pawprintData.public ? chalk.bold.cyan('[Public]') :
+                            chalk.bold.yellow('[Private]')));
   log(chalk.gray('    Share Link: ') + chalk.underline(pawprintData.public_link));
-  log('')
-  log(chalk.gray('    Embed as an iframe:'))
-  log('    <iframe src="' + pawprintData.embed_link + '"></iframe>')
-  log('')
-  log(chalk.gray('    To delete this Pawprint'))
-  log('    curl -X DELETE ' + pawprintData.delete_link)
+  log('');
+  if (isCopied) {
+    log(chalk.bold.green('  ✓ Link copied to clipboard'));
+    log('');
+  }
+  log(chalk.gray('    Embed as an iframe:'));
+  log('    <iframe src="' + pawprintData.embed_link + '" width="100%" height="500" style="border: solid 1px #cad5db; border-radius: 2px; max-width: 700px"></iframe>');
+  log('');
+  log(chalk.gray('    To delete this Pawprint'));
+  log('    curl -X DELETE ' + pawprintData.delete_link);
 };
 
 var logError = function(errorMessage) {
-  log('')
-  log(chalk.red.bold('  𐄂 Error'))
-  var lines = errorMessage.split('\n')
+  log('');
+  log(chalk.red.bold('  𐄂 Error'));
+  var lines = errorMessage.split('\n');
   for (var i = 0; i < lines.length; i++) {
-    log(chalk.red('    ' + lines[i]))
+    log(chalk.red('    ' + lines[i]));
   }
 }
 
-var createPawprint = function(httpRequest, httpResponse, isPrivate) {
+var createPawprint = function(httpRequest, httpResponse, isPrivate, cb) {
   var isPublic = true;
   if (isPrivate === true) {
     isPublic = false;
@@ -47,35 +50,55 @@ var createPawprint = function(httpRequest, httpResponse, isPrivate) {
     }
   }, (error, response, body) => {
     if (!error && response.statusCode >= 200 && response.statusCode < 300) {
-      logPawprintCreation(body);
+      cb(true, body);
       return;
     }
-    logError('Cannot create Pawprint, an error occured with the Pawprint API.')
+    cb(false);
   })
 };
 
-cmd.description('Utility tool to create and manage Pawprints HTTP traces.') 
-   .version('1.0.0')
-   .option('-p, --private', 'Create a private Pawprint')
-   .parse(process.argv);
+var pawprintCli = function(cmd) {
+  var process = require('process');
+  var ncp = require("copy-paste");
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+  cmd.description('Utility tool to create and manage Pawprints HTTP traces.') 
+     .version('1.0.0')
+     .option('-p, --private', 'Create a private Pawprint')
+     .option('-c, --copy', 'Copy the share link to clipboard')
+     .parse(process.argv);
 
-var trace = '';
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (chunk) => {
-  trace += chunk;
-});
+  var trace = '';
 
-process.stdin.on('end', () => {
-  var p = parser.parse(trace);
-  if (typeof p.request !== 'string' || typeof p.response !== 'string' ||
-      p.request === '' || p.response === '') {
-    logError('Invalid curl trace.\n' +
-             'Try: curl --trace - http://httpbin.org/get | pawpt\n' +
-             'Note that --trace should be followed by a dash (--trace -)')
-    return;
-  }
-  createPawprint(p.request, p.response, cmd.private);
-});
+  process.stdin.on('data', (chunk) => {
+    trace += chunk;
+  });
+
+  process.stdin.on('end', () => {
+    var p = parser.parse(trace);
+    if (typeof p.request !== 'string' || typeof p.response !== 'string' ||
+        p.request === '' || p.response === '') {
+      logError('Invalid curl trace.\n' +
+               'Try: curl --trace - http://httpbin.org/get | pawpt\n' +
+               'Note that --trace should be followed by a dash (--trace -)');
+      return;
+    }
+    createPawprint(p.request, p.response, cmd.private, (success, pawprintData) => {
+      if (success) {
+        if (cmd.copy) {
+          ncp.copy(pawprintData.public_link);
+        }
+        logPawprintCreation(pawprintData, cmd.copy);
+      }
+      else {
+        logError('Cannot create Pawprint, an error occured with the Pawprint API.');
+      }
+    });
+  });
+};
+
+if (require.main === module) {
+  pawprintCli(require('commander'));
+}
